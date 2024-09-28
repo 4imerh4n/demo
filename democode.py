b51182f94db471ca8e2ace1eda4b5f524164ba98
4imerh4n <lnh.edu.279@gmail.com>
2024-09-28 08:34:22 +0700

    Update democode.py

--- democode.py
+++ democode.py
@@ -2,16 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Create a sample dataset
 data = {'Category': ['A', 'B', 'C', 'D', 'E'], 
         'Value': [10, 20, 30, 40, 50]}
 df = pd.DataFrame(data)
-
-# Create a bar chart
 fig, ax = plt.subplots()
 ax.bar(df['Category'], df['Value'])
-
-# Create a Streamlit app
-st.title("Bar Chart Example")
-st.write("This is a simple bar chart example on Streamlit.")
 st.pyplot(fig)
