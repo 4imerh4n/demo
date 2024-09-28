1e734bfd7efe437d5c00c04c7c871ee47132542e
4imerh4n <lnh.edu.279@gmail.com>
2024-09-28 08:26:18 +0700

    Update democode.py

--- democode.py
+++ democode.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+df = pd.read_csv('Dubai.csv')
 sns.set_theme(font = 'serif')
 plt.figure(figsize=(8,7))
 sns.heatmap(corr_matrix,annot=True,cmap='PuBuGn',linewidths=.5)
